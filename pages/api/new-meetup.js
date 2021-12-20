6306cf2c1992d558a493af4eeeddc5e3de904f61
sreyom31 <sreyom31@gmail.com>
2021-12-20 18:08:56 +0530

    sending http routes and storing in mongodb
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -2,11 +2,12 @@ import { MongoClient } from "mongodb";
 
 const handler = async (req, res) => {
     if(req.method === 'POST') {
-        const { title, image, address, description } = req.body
+        const data = req.body
+        const { title, image, address, description } = data
         const client = await MongoClient.connect(`mongodb+srv://admin-sreyom:${process.env.MONGO_PASS}@cluster0.8fstb.mongodb.net/meetups?retryWrites=true&w=majority`)
         const db = client.db()
         const meetupsCollection = db.collection('meetups')
-        const result = await meetupsCollection.insertOne(req.body)
+        const result = await meetupsCollection.insertOne(data)
         console.log(result)
         client.close()
         res.status(201).json({message: 'Meetup Inserted'})
