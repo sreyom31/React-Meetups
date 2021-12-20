798a16922d6141dff7653d5558ae8c1f87146bf6
sreyom31 <sreyom31@gmail.com>
2021-12-20 17:47:19 +0530

    working with mongodb
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,5 +1,16 @@
-const handler = (req, res) => {
+import { MongoClient } from "mongodb";
+
+const handler = async (req, res) => {
     if(req.method === 'POST') {
         const { title, image, address, description } = req.body
+        const client = await MongoClient.connect(`mongodb+srv://admin-sreyom:${process.env.MONGO_PASS}@cluster0.8fstb.mongodb.net/meetups?retryWrites=true&w=majority`)
+        const db = client.db()
+        const meetupsCollection = db.collection('meetups')
+        const result = await meetupsCollection.insertOne(req.body)
+        console.log(result)
+        client.close()
+        res.status(201).json({message: 'Meetup Inserted'})
     }
 }
+
+export default handler
