b184f5f0d93fd4c993f60ee5a532c9d5033138e8
sreyom31 <sreyom31@gmail.com>
2021-12-20 14:29:15 +0530

    pre-rendering and its problems
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import MeetupList from '../components/meetups/MeetupList';
+import {useEffect, useState} from "react";
 
 const DUMMY_MEETUPS = [
     {
@@ -18,7 +19,12 @@ const DUMMY_MEETUPS = [
 ];
 
 function HomePage() {
-    return <MeetupList meetups={DUMMY_MEETUPS} />
+    const [loadedMeetups, setLoadedMeetups] = useState([]);
+    useEffect(() => {
+        setLoadedMeetups(DUMMY_MEETUPS)
+    }, []);
+
+    return <MeetupList meetups={loadedMeetups} />
 }
 
 export default HomePage;
