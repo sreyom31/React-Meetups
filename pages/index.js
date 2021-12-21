d6dcce60a95672b99893ac555c85107514514062
sreyom31 <sreyom31@gmail.com>
2021-12-21 11:12:17 +0530

    getting data from db
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { MongoClient } from "mongodb";
 import MeetupList from '../components/meetups/MeetupList';
 
 const DUMMY_MEETUPS = [
@@ -35,9 +36,22 @@ function HomePage(props) {
 
 export async function getStaticProps() {
     // fetch data from an API
+    const client = await MongoClient.connect(`mongodb+srv://admin-sreyom:${process.env.MONGO_PASS}@cluster0.8fstb.mongodb.net/meetups?retryWrites=true&w=majority`)
+    const db = client.db()
+    const meetupsCollection = db.collection('meetups')
+    const meetups = await meetupsCollection.find().toArray();
+    client.close()
+
     return {
         props: {
-            meetups: DUMMY_MEETUPS
+            meetups: meetups.map((meetup) => {
+                return {
+                    title: meetup.title,
+                    address: meetup.address,
+                    image: meetup.image,
+                    id: meetup._id.toString(),
+                }
+            })
         },
         revalidate: 10 // regenerates the page in every 10 seconds.
     };
